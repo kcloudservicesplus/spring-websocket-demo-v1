e69b08b102f31289999c3d423ec9e337a5d7b3c4
Kapil Patil <kcloudservicesplus@gmail.com>
2022-04-06 00:12:38 -0400

    Initial commit

---
+++ src/main/java/com/kapilpatil84/springwebsocketdemo/SpringWebsocketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kapilpatil84.springwebsocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketDemoApplication.class, args);
+    }
+
+}
